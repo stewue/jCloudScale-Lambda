fe0c1067fcf4e0ce181108b85cbf120ddc9fc045
Stefan Würsten <stefan@wuersten.ch>
2017-03-03 08:28:03 +0100

    Enhance code skeleton

--- code/src/main/java/ch/uzh/ifi/seal/jcs_lambda/management/CloudMethodEntity.java
+++ code/src/main/java/ch/uzh/ifi/seal/jcs_lambda/management/CloudMethodEntity.java
@@ -1,6 +1,7 @@
 package ch.uzh.ifi.seal.jcs_lambda.management;
 
-public class CloudMethodObject {
+public class CloudMethodEntity {
     private String name;
     private String url;
+    private Object arguments;
 }
