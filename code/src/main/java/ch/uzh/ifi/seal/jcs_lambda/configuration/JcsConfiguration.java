f3e84f9b6bca90ad8076fc4907f9c6b12610d8f4
Stefan Würsten <stefan@wuersten.ch>
2017-03-04 11:12:33 +0100

    Add more configuration fields

--- code/src/main/java/ch/uzh/ifi/seal/jcs_lambda/configuration/JcsConfiguration.java
+++ code/src/main/java/ch/uzh/ifi/seal/jcs_lambda/configuration/JcsConfiguration.java
@@ -1,13 +1,12 @@
 package ch.uzh.ifi.seal.jcs_lambda.configuration;
 
+import com.amazonaws.regions.Regions;
+
 public class JcsConfiguration {
-    /*
-    Possible Configs:
-    - AWS
-        - region
-        - Access Key ID
-        - Secret Access Key
-        - default memory
-        - default timeout
-     */
+    public static final String AWS_ACCESS_KEY_ID = "HERE ACCESS KEY ID";
+    public static final String AWS_SECRET_KEY_ID = "HERE SECRET KEY ID";
+    public static final Regions AWS_REGION = Regions.EU_CENTRAL_1;
+    public static final int AWS_TIMEOUT = 3;
+    public static final int AWS_DEFAULT_MEMORY_SIZE = 128;
+    public static final String AWS_ROLE_NAME = "JCS-Lambda";
 }
