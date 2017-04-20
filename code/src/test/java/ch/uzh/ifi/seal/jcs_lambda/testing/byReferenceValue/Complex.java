da9d51f82d090680027a540470f2827dfcc388bb
stewue <stefan@wuersten.ch>
2017-04-20 14:30:07 +0200

    add by reference test case

---
+++ code/src/test/java/ch/uzh/ifi/seal/jcs_lambda/testing/byReferenceValue/Complex.java
@@ -0,0 +1,8 @@
+package ch.uzh.ifi.seal.jcs_lambda.testing.byReferenceValue;
+
+import java.util.Map;
+
+public class Complex {
+    public String y;
+    public Map<String, Integer> z;
+}
---
+++ code/src/test/java/ch/uzh/ifi/seal/jcs_lambda/testing/byReferenceValue/TestObject.java
@@ -0,0 +1,34 @@
+package ch.uzh.ifi.seal.jcs_lambda.testing.byReferenceValue;
+
+import ch.uzh.ifi.seal.jcs_lambda.annotations.ByReference;
+import ch.uzh.ifi.seal.jcs_lambda.annotations.CloudMethod;
+import ch.uzh.ifi.seal.jcs_lambda.annotations.Local;
+import ch.uzh.ifi.seal.jcs_lambda.annotations.ReadOnly;
+
+import java.util.UUID;
+
+public class TestObject {
+    public String _uuid_ = UUID.randomUUID().toString();
+
+    @ReadOnly
+    private int a;
+
+    @ByReference
+    private Complex b;
+
+    public void initialize( int a, Complex b){
+        this.a = a;
+        this.b = b;
+    }
+
+    @CloudMethod( timeout = 15, memory = 512 )
+    public int doSomething(){
+        System.out.println("Cloud only");
+        System.out.println( "IS NULL: " + b.y );
+
+        b.y = "Modified";
+        b = new Complex();
+
+        return a * 2;
+    }
+}
