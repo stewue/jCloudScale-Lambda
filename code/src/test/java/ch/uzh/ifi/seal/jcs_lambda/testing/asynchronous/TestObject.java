aff332879c8a624ba22d42a5225402373ee33336
stewue <stefan@wuersten.ch>
2017-05-07 11:17:12 +0200

    asynchronous tests

---
+++ code/src/test/java/ch/uzh/ifi/seal/jcs_lambda/testing/asynchronous/TestObject.java
@@ -0,0 +1,14 @@
+package ch.uzh.ifi.seal.jcs_lambda.testing.asynchronous;
+
+import ch.uzh.ifi.seal.jcs_lambda.annotations.CloudMethod;
+import ch.uzh.ifi.seal.jcs_lambda.annotations.Local;
+import ch.uzh.ifi.seal.jcs_lambda.annotations.ReadOnly;
+import ch.uzh.ifi.seal.jcs_lambda.logging.Logger;
+
+public class TestObject {
+    @CloudMethod
+    public int sum( int a, int b ) throws Exception{
+        Logger.info("Cloud only");
+        return a + b;
+    }
+}
