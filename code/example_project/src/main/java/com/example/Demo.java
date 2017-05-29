270169963fa71b118c942d7b3f46cfe3e159a883
stefan.wuersten@uzh.ch <$taefa99>
2017-05-29 15:22:18 +0200

    fix problem with gateway api release and that the endpoint was not immediately available

--- code/example_project/src/main/java/com/example/Demo.java
+++ code/example_project/src/main/java/com/example/Demo.java
@@ -8,12 +8,12 @@ public class Demo
     @StartUp
     public static void main ( String [] args ){
         Demo demo = new Demo();
-        System.out.println( demo.run() );
+
+        System.out.println( demo.run78( 99 ) );
     }
 
     @CloudMethod
-    public String run(){
-        System.out.println("cloud only");
-        return "Run code in cloud";
+    public String run78( int a ){
+        return "Run code in cloud iiiiiiiiiiiii" + a;
     }
 }
