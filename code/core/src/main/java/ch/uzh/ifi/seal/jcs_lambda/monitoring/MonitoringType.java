2cb47a842fb298a3d7a9896c04b64d9333cbd06e
stewue <stefan@wuersten.ch>
2017-06-11 13:52:25 +0200

    add monitoring functionality

---
+++ code/core/src/main/java/ch/uzh/ifi/seal/jcs_lambda/monitoring/MonitoringType.java
@@ -0,0 +1,12 @@
+package ch.uzh.ifi.seal.jcs_lambda.monitoring;
+
+public enum MonitoringType {
+    TOTAL,
+    AWS_INIT,
+    CODE_MODIFICATION,
+    MAVEN,
+    UPLOAD,
+    CONFIGURE_ENDPOINTS,
+    ENDPOINT_AVAILABLE,
+    REMOVE_BUCKET
+}
