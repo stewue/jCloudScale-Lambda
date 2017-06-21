bb417824a9e161691ec96a885b383d34267c2bae
stefan.wuersten@uzh.ch <$taefa99>
2017-06-21 17:30:46 +0200

    case study

---
+++ code/evaluation/monte_carlos_pi_calculation/cloud_project/src/main/java/pi/MyThread.java
@@ -0,0 +1,63 @@
+package pi;
+
+import ch.uzh.ifi.seal.jcs_lambda.annotations.CloudMethod;
+import ch.uzh.ifi.seal.jcs_lambda.annotations.ReadOnly;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class MyThread implements Callable<Long> {
+	@ReadOnly
+	private int nameThread;
+	@ReadOnly
+	private long sideSquare;
+	@ReadOnly
+	private long pointsOfThread;
+	@ReadOnly
+	private long numPointsInCircle;
+	@ReadOnly
+	private boolean quit;
+
+	public MyThread (){
+
+	}
+
+	public MyThread(int nameThread, long sideSquare, long pointsOfThread, boolean quit) {
+		this.nameThread = nameThread;
+		this.sideSquare = sideSquare;
+		this.pointsOfThread = pointsOfThread;
+		this.quit = quit;
+	}
+
+	@CloudMethod( memory = 1536, timeout = 30 )
+	public Long call() {
+		if (!quit) {
+			System.out.println("Thread" + nameThread + " started" + "Points " + pointsOfThread + " !");
+		}
+		numPointsInCircle = 0l;
+		for (long i = 0; i < pointsOfThread; i++) {
+			long x = randomNumber(0, sideSquare);
+			long y = randomNumber(0, sideSquare);
+			Point point = new Point(x, y);
+			if (isInCircle(sideSquare / 2, point))
+				numPointsInCircle++;
+		}
+		if (!quit) {
+			System.out.println(nameThread + " is finishing! Points in circle: " + numPointsInCircle + " !");
+		}
+		return numPointsInCircle;
+	}
+
+	private boolean isInCircle(long radius, Point point) {
+		long distance = (radius - point.getX()) * (radius - point.getX())
+				+ (radius - point.getY()) * (radius - point.getY());
+		if (distance > radius * radius)
+			return false;
+		else
+			return true;
+	}
+
+	private long randomNumber(long min, long max) {
+		return ThreadLocalRandom.current().nextLong(min, max + 1);
+	}
+}
---
+++ code/evaluation/monte_carlos_pi_calculation/cloud_project/src/main/java/pi/Pi.java
@@ -0,0 +1,66 @@
+package pi;
+
+import ch.uzh.ifi.seal.jcs_lambda.annotations.StartUp;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.concurrent.*;
+
+public class Pi {
+
+	@StartUp( deployToCloud = false )
+	public static void main(String[] args) {
+		int numThread = 0;
+		long sideSquare = 0;
+		boolean quit = false;
+
+		long timeOfStart = Calendar.getInstance().getTimeInMillis();
+		for (int i = 0; i < args.length; i++) {
+			if (args[i].equals("-s")) {
+				sideSquare = new Long(args[i + 1]);
+			}
+			if (args[i].equals("-t")) {
+				numThread = new Integer(args[i + 1]);
+			}
+			if (args[i].equals("-q")) {
+				quit = true;
+			}
+		}
+		if (sideSquare <= 0 || numThread <= 0 || args.length < 4 || args.length > 5) {
+			System.out.println("ERROR: Args are not correct!!!");
+			return;
+		} else {
+			ExecutorService executor = Executors.newFixedThreadPool(numThread);
+			CompletionService<Long> pool = new ExecutorCompletionService<Long>(executor);
+			List<Future<Long>> futures = new ArrayList<Future<Long>>(numThread);
+			long numPoints = sideSquare * sideSquare;
+			long pointsOfThread = numPoints / numThread;
+			for (int t = 0; t < numThread; t++) {
+				if (t == numThread - 1) {
+					pointsOfThread = numPoints - pointsOfThread * (numThread - 1);
+				}
+				futures.add(pool.submit(new MyThread(t, sideSquare, pointsOfThread, quit)));
+			}
+
+			long pointsInCircle = 0;
+			for (Future<Long> future : futures) {
+				try {
+					pointsInCircle += future.get();
+				} catch (InterruptedException | ExecutionException e) {
+					e.printStackTrace();
+				}
+			}
+			double pi = 4 * ((double) pointsInCircle / numPoints);
+			long timeOfEnd = Calendar.getInstance().getTimeInMillis();
+			if (!quit) {
+				System.out.println("Points in square: " + numPoints);
+				System.out.println("Points in circle: " + pointsInCircle);
+			}
+			System.out
+					.println("Total execution time for current run (millis): " + (timeOfEnd - timeOfStart) + " millis");
+			System.out.println("Calculate pi.Pi: " + pi);
+			executor.shutdown();
+		}
+	}
+}
