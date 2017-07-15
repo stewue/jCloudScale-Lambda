471cf03fdc6e21edfb218fca1e2afaa26ef66dd2
stefan.wuersten@uzh.ch <$taefa99>
2017-07-15 16:58:34 +0200

    update gitignore

---
+++ code/evaluation/quantitative/5.2_5.8_matrix_multiplication_jCloudScale_Lambda/src/main/java/com/matrix_multiplication/Calculator.java
@@ -0,0 +1,55 @@
+package com.matrix_multiplication;
+
+import ch.uzh.ifi.seal.jcs_lambda.annotations.CloudMethod;
+import ch.uzh.ifi.seal.jcs_lambda.annotations.StartUp;
+
+public class Calculator
+{
+    @StartUp
+    public static void main ( String [] args ){
+        int m = 1000;
+        int n = 1000;
+        int o = 1000;
+
+        for(int i=0; i<1; i++) {
+            Matrix matrixA = new Matrix(m, n);
+            matrixA.setRandomValues();
+
+            Matrix matrixB = new Matrix(n, o);
+            matrixB.setRandomValues();
+
+            long startTime = System.currentTimeMillis();
+            Matrix matrixResult = matrixMultiplication(matrixA, matrixB);
+            System.out.println( (System.currentTimeMillis() - startTime) );
+        }
+    }
+
+    @CloudMethod( memory = 1024, timeout = 45 )
+    public static Matrix matrixMultiplication( Matrix a, Matrix b ){
+        int aY = a.getY();
+        int aX = a.getX();
+        int bY = b.getY();
+        int bX = b.getX();
+
+        // check dimension of matrix
+        if( aX != bY ){
+            throw new IllegalArgumentException();
+        }
+
+        Matrix result = new Matrix( aY, bX );
+
+        for( int y=0; y<aY; y++ ){
+            for( int x=0; x<bX; x++ ){
+                int sum = 0;
+
+                for( int c = 0; c<aX; c++ ){
+                    sum += a.getElement(y, c) * b.getElement(c, x);
+                }
+
+                result.setElementValue( y, x, sum );
+            }
+        }
+
+        return result;
+    }
+}
