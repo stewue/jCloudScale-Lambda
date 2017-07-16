f3d33649a8ae377cacbd41cf1b61a06f56d15bba
stewue <stefan@wuersten.ch>
2017-07-16 14:18:10 +0200

    git tests

---
+++ code/core/src/test/java/ch/uzh/ifi/seal/jcs_lambda/testing/byReferenceValueExplicit/TestObject.java
@@ -0,0 +1,38 @@
+package ch.uzh.ifi.seal.jcs_lambda.testing.byReferenceValueExplicit;
+
+import ch.uzh.ifi.seal.jcs_lambda.annotations.ByReference;
+import ch.uzh.ifi.seal.jcs_lambda.annotations.CloudMethod;
+import ch.uzh.ifi.seal.jcs_lambda.annotations.ReadOnly;
+import ch.uzh.ifi.seal.jcs_lambda.cloudprovider.byReference.Explicit;
+import ch.uzh.ifi.seal.jcs_lambda.logging.Logger;
+import com.google.gson.Gson;
+
+public class TestObject {
+    @ReadOnly
+    private int a;
+
+    @ByReference
+    private Complex b;
+
+    public void initialize( int a, Complex b){
+        this.a = a;
+        this.b = b;
+    }
+
+    @CloudMethod( timeout = 15, memory = 512 )
+    public int doSomething(){
+        Logger.info("Cloud only");
+
+        Explicit.get( this, "b" );
+
+        b.y = "TEST STRING";
+
+        Explicit.set( this, "b" );
+
+        return a * 2;
+    }
+
+    public Complex getB (){
+        return b;
+    }
+}
---
+++ code/core/src/test/java/ch/uzh/ifi/seal/jcs_lambda/testing/genericType/Complex.java
@@ -0,0 +1,11 @@
+package ch.uzh.ifi.seal.jcs_lambda.testing.genericType;
+
+public class Complex {
+    int a;
+    int b;
+
+    public Complex ( int a, int b ){
+        this.a = a;
+        this.b = b;
+    }
+}
