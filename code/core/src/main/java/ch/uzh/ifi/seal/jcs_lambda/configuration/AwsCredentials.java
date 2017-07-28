3195fdafce88f33cb8d495c1cf7d1fe40bf06fde
stefan.wuersten@uzh.ch <$taefa99>
2017-07-28 10:36:59 +0200

    remove config

--- code/core/src/main/java/ch/uzh/ifi/seal/jcs_lambda/configuration/AwsCredentials.java
+++ code/core/src/main/java/ch/uzh/ifi/seal/jcs_lambda/configuration/AwsCredentials.java
@@ -1,6 +1,6 @@
 package ch.uzh.ifi.seal.jcs_lambda.configuration;
 
 public class AwsCredentials {
-    public static final String AWS_ACCESS_KEY_ID = "AKIAJFO7OZICGILIKDGQ";
-    public static final String AWS_SECRET_KEY_ID = "NJ00/9vY+viAvsX1Y05HZmfXBRDVag4ZQmFQBKFA";
+    public static final String AWS_ACCESS_KEY_ID = "HERE YOUR ACCESS KEY";
+    public static final String AWS_SECRET_KEY_ID = "HERE YOUR SECRET KEY";
 }
